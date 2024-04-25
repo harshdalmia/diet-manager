d79d048f18b0a3e5a2e6f29047b502a84493cb75
Ekaansh-Jain <156109728+Ekaansh-Jain@users.noreply.github.com>
2024-04-25 13:14:35 +0530

    Main cpp file with correctioin
    All the source files are now correctly interconnected.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,71 @@
+#include <iostream>
+#include "DietInput.cpp"
+#include "dietplan.cpp"
+#include "usserdiet.cpp"
+#include "drinkingwater.cpp"
+using namespace std;
 
+int main()
+{   
+    int choice;
+    cout<<"Hello User!, Please Enter Your details"<<endl;
+    dietdetails d;
+    d.inputUserDetails();
+    d.determineDietPlan();
+    cout<<d.fname<<", You are "<<d.goal<<endl;
+    string goal=d.goal;
+    bool veg=d.veg;
+    DietType *s;
+    bool isok=false;
+    while(!isok)
+    {
+    cout << "Please Select an option below:" << endl;
+    cout << "1. Create your own diet plan" << endl;
+    cout << "2. Suggestion of our diet plan" << endl;
+    cout<< "3. Or would you like to proceed with drinking water recording system "<<endl;
+    cout << "4. Exit" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    
+    if(choice==2)
+    {
+        
+        if(veg==true){
+            VegDiet a(goal);
+            a.printDietPlan();
+        }
+        else {
+            NonVegDiet a(goal);
+            a.printDietPlan();
+        }
+        isok=true;
+        
+
+        }
+        
+    if(choice==1)
+    {
+        breakfast();
+        lunch();
+        snacks();
+        dinner();
+        display(goal);
+        isok=true;
+    }
+    if(choice==4)
+    {
+        cout<<"Successfully Exited...";
+        isok=true;
+    }
+    if(choice==3)
+    {
+        manageWater();
+    }
+    if(choice<=0||choice>4)
+    {
+        cout<<"Invalid Input, Please try again"<<endl;
+        isok=false;
+    }
+    }
+}
